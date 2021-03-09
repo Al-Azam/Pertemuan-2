03794a195d7b5b438cabf4c338558662752f7b9a
Al-Azam <al.azam@students.amikom.ac.id>
2021-03-09 16:19:30 +0700

    Penambahan Tugas poin 4

--- Calculator.cs
+++ Calculator.cs
@@ -2,8 +2,16 @@ class Calculator
 {
 	// Tugas 4
 	static void Main(string[] args)
-	{}
+	{
+		int a = 10;
+		int b = 6;
+		
+		console.writeline("Hasil Penambahan {0} + {1} = {2}, a, b, Penambahan(a,b));
+		console.writeline("Hasil Pengurangan {0} - {1} = {2}, a, b, Pengurangan(a,b);
 	
+		console.writeline("\n Tekan sembarang tombol untuk keluar");
+		console.Readkey();
+	}
 	
 	
 	// Tugas 2
