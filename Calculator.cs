5d56a34d14934f962a0457563500243682f054f4
Al-Azam <al.azam@students.amikom.ac.id>
2021-03-09 16:14:17 +0700

    Penambahan fungsi writeline

--- Calculator.cs
+++ Calculator.cs
@@ -1,10 +1,24 @@
 class Calculator
 {
-static void Main(string[] args)
+	// Tugas 4
+	static void Main(string[] args)
+	{}
 	
+	
+	
+	// Tugas 2
 	static int Penambahan(int a, int b)
 	{
 	return a + b;
 	}
 
+	// Tugas 3
+	static int Pengurangan(int a, int b)
+	{
+	return a - b;
+	}
+	
 }
+	
+	
+
